a46261066d0c3d7bc542682979f3fb234c7cf1bc
ashish-az <ay053658@gmail.com>
2024-12-25 23:06:32 +0000

    new edit

---
+++ 01_basics/data.types.js
@@ -0,0 +1,77 @@
+//primitive 
+// 7 types of  == string,number,null,boolean,undefined,symbol,bigInt
+
+//reference/non-primitive 
+// array,objects,functions 
+
+
+// Primitive DataTypes:
+
+
+// 1.Number: Represents both integer and floating-point numbers. Example: 5, 3.14.
+//1.Number:
+// Represents both integers and floating-point numbers.
+ 
+// 2.String: Represents a sequence of characters enclosed in single or double quotes. Example: "Hello, world!".
+//  2.String:
+// Represents a sequence of characters.
+
+// 3,Boolean: Represents a binary value, either true or false, often used for conditional logic.
+//3.Boolean:
+// Represents true or false.
+// Only two possible values: true and false. 
+
+// 4.Undefined: Represents a variable that has been declared but hasn't been assigned a value yet. Example: let x;.
+// 4.Undefined:
+// Represents a variable that has been declared but hasn't been assigned a value.
+// It has only one possible value: undefined.
+
+// 5.Null:  standalone value it is represenataion of empty value it is special type and it is a object type. Example: let y = null; 
+// 5.Null:
+// Represents the intentional absence of any object or value.
+// It has only one possible value: null.
+
+// 6.Symbol (ES6): it is mostly used used to find uniqnece.
+// 6.Symbol (ES6):
+// Represents a unique and immutable value.
+// No specific range limit.
+
+// 7.BigInt (ES11): Represents large integers that cannot be represented by the Number type.
+// 7.BigInt (ES11):
+// Represents large integers that cannot be represented by the Number type.
+// The range is practically unlimited and depends on available memory.
+
+
+//  Refence (Object Data Types):
+
+// 1.Object: Represents a collection of key-value pairs (properties and methods). Example: { name: "John", age: 30 }.
+
+// 2.Array: Represents a list-like collection of values, indexed by numbers (integer indices). Example: [1, 2, 3, 4].
+
+// 3.Function: Represents a reusable block of code that can be invoked or called with arguments. Example: function add(x, y) { return x + y; }.
+
+// 4.Date: Represents dates and times.
+
+// 5.RegExp: Represents regular expressions for pattern matching.
+
+
+// Premitive Datatypes
+
+// Type                                   typeof
+
+// i)             Number                               number
+// ii)            String                                   string
+// iii)           Boolean                               boolean
+// iv)           Bigint                                   bigint
+// v)            Symbol                                symbol
+// vi)           Null                                      object
+// vii)          Undefined                           undefined
+
+
+// * Non-Premitive OR Referance OR Object datatype
+
+// Type                                   typeof
+
+// i)               Object                                object
+// ii)              Array                                  object
+// iii)             Function                            function(object)
