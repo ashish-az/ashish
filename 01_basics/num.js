0f0377fa7cba42b4667b7fa77183d81a77b1f52e
ashish-az <ay053658@gmail.com>
2025-01-18 17:56:24 +0000

    num

--- 01_basics/num.js
+++ 01_basics/num.js
@@ -1,1 +1,18 @@
-const balance = 200
+// const balance = 200
+// console.log(balance);
+
+// const score = new Number(20990)
+// console.log(score);
+
+// console.log(score.toString().length);
+// console.log(balance.toFixed(3));
+
+// const otherNumber = 1234.16545
+// console.log(otherNumber.toPrecision(6
+// ));
+
+// const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+// +++++ maths +++++ //
