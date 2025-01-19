e0ce8eae971e8d7048a14018ab7405611b44779b
ashish-az <ay053658@gmail.com>
2025-01-19 20:22:16 +0000

    num

--- 01_basics/num.js
+++ 01_basics/num.js
@@ -29,4 +29,5 @@ console.log(Math.random()*10) + 1;
 const min = 120
 const max = 200
 
-console.log(Math.floor(Math.random() * (max-min + 1)) + min)
+// console.log(Math.floor(Math.random() * (max-min + 1)) + min)
+               
