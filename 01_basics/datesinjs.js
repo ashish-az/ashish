f611bc8e61091b0e895cbbb1079201353d8d5c44
ashish-az <ay053658@gmail.com>
2025-01-21 21:51:28 +0000

    dates

--- 01_basics/datesinjs.js
+++ 01_basics/datesinjs.js
@@ -1,3 +1,16 @@
 // dates in javascript
 
-let Date = new Date
+let mydate = new Date()
+console.log(myDate.tostring());
+
+to datestring
+to isosting
+to JSON
+to localdatestring
+to localtimestring
+to utcstring
+
+date is an object.
+
+let myCreatedDate = new Date(2025, 12, 22)
+console.log(myCreatedDate);
